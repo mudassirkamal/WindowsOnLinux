0136df21ad755bd79c837954a3899f0756493fab
Mudassir Kamal <72467695+mudassirkamal@users.noreply.github.com>
2025-02-11 19:30:15 +0500

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]; then
   echo "Run as root!"
   exit 1
 fi
-IMAGE_URL="https://yourdomain.com/win2019.gz"
+IMAGE_URL="https://archive.org/download/win-10.-pro_20240204/WIN10.PRO.gz"
 echo "Downloading Windows image..."
 wget -O win2019.gz $IMAGE_URL || curl -L $IMAGE_URL -o win2019.gz
 echo "Installing Windows..."
